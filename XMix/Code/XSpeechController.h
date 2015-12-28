ce236c7d5cb8d41eb12b610546698adb0ceb43e8
Davidli <macmafia@sina.cn>
2015-12-28 15:44:56 +0800

    阅读器

---
+++ XMix/Code/XSpeechController.h
@@ -0,0 +1,13 @@
+//
+//  XSpeechController.h
+//  XMix
+//
+//  Created by davidli on 15/12/28.
+//  Copyright © 2015年 X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XSpeechController : UIViewController
+
+@end
